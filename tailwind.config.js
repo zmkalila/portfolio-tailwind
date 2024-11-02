efb903cd23d7faa023614c1d87b6663c2fbceb15
zmkalila <zmkalila@gmail.com>
2024-11-02 18:06:23 +0700

    Add dark mode toggle, add button that can scroll page all the way to the top, and minify CSS file size

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html"],
+  darkMode: "class",
+  // ganti jadi "media" kalau mau dark/light mode ngikutin setting os masing-masing user
   theme: {
     container: {
       center: true,
@@ -10,7 +12,8 @@ module.exports = {
       colors: {
         primary: "#38bdf8", // db2777
         secondary: "#64748b",
-        dark: "0f172a",
+        dark: "#0f172a",
+        darkmode: "#1e293b", // 1c1c22
       },
       screens: {
         "2xl": "1320px",
